0f9e86be138c89e7a5fe7624e22d2fd01219a632
Angelo Andrade <angelo1367rocha@gmail.com>
2024-09-24 09:49:30 -0300

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,75 @@
+reposta = [
+    ["Mario", "Sonic Mania", "GTA VI", "Dr Ivo Robotnik", "The legend of Zelda: Ocarina of time" "Sonic" "Crash"],
+    ["Red dead redemption 2", "Pokemon", "Call of Duty", "Hyper Sonic", "Mario Mario" "Sonic Forces" "YU-GI-OH!!!!", "Dr eggman", "DR Gerald"],
+    ["Mario bros", "Super sonic", "Dark sonic", "The legend of Zelda breath of the wild", "Battlefield V", "battlefield IV"],
+    ["Hyper Sonic", "Mario Mario" "Sonic Generation" "YU-GI-OH!!!!", "Dr eggman", "DR Gerald", "GTA IV", " The Last of Us Part 2",],
+    ["Marios Bros" "Mario Luigi", "Bully 2",  "Red dead redempetion 3"]
+]
+print(reposta)
+
+reposta1 = [
+    ["Crash", "Sonic", "Mario"]
+            ]
+print(reposta1)
+p = str(input("Qual mascote mais famoso dos games")) 
+if p == "Mario":
+    print("pergunta2")
+    reposta = [ [ "Mario", "YU-GI-OH!!!", "Pokemon"] ]
+    print(reposta)
+    p = str(input("Qual o jogo foi mais polemico nos anos 80"))
+else: print("Tente novamente")
+if p == "Pokemon":
+    print("Pergunta 3")
+    reposta = [["Dr eggman", "Dr Ivo Robotnik", "Dr gerald"]]
+    print(reposta)
+    p = str(input("Qual é o verdadeiro nome de Dr. eggman"))
+else: print("Tente novamente")
+if p == "Dr Ivo Robotnik":
+    print("Pergunta 4")
+    reposta = [["Super sonic", "Dark Sonic","Hyper Sonic"]]
+    print (reposta)
+    p= str(input("Qual trasformação mais poderosa do Sonic"))
+else: print("Tente novamente")
+if p == "Hyper Sonic":
+    print("pergunta 5")
+  
+    reposta [["Sonic Mania", "Sonic Forces", "Sonic Generation"]]
+    print(reposta)
+    p = str(input("Qual jogo que triuxe o retorno definitivo do Sonic classico"))
+else:
+    print("Tente novamente")
+if p  == "Sonic Mania":
+    print("pergunta 6")
+   
+    reposta = [[" The Last of Us Part 2", "Red dead redemption 2", "GTA IV"]]
+    print(reposta)
+    p = str(input("Qual o jogo que é considerado um dos mais realistas"))
+else: print("Tente novamente")
+if p == "Red dead redemption 2":
+    print("Pergunta 7")
+    reposta = [["The legend of Zelda breath of the wild", "The legend of Zelda: Ocarina of time" "GtA IV"]] 
+    print(reposta)
+    p == str(print("Qual jogo é considerado uma obra de arte com 99 de nota no metacritic"))
+
+else: print("Tente novamente")
+if p == "The legend of zelda: Ocarina of time":
+    print("Pergunta 8")
+  
+    reposta = [["Mario Mario", "Marios Bros", "Mario Luigi"]]
+    print(reposta)
+    p == str(print("qual o nome completo de mario"))
+else: print("Tente novamente")
+if p == "Mario Mario":
+    print("Pergunta 9")
+    reposta = [["Call pf Duty", "Battlefield V", "Battlefield IV"]]
+    print(reposta) 
+    p = str(input("Qual é a franquia de jogos de guerra mais popular"))
+else: print("Tente novamente")
+if p == "Call of Duty":
+    print("Pergunta 10") 
+    reposta = [["GTA VI", "Red dead redemption 3", "Bully 2"]] 
+    p = str(input("Qual o jogo mais a guardado dos ultimos anos"))
+else: print("Tente novamente")
+if p == "GTA VI":
+    print("Parabens você concluiu o Quiz")
+
